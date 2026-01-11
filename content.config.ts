1128b7106c12424ed48b39204e75baa9f89e70dd
samuelreichor <samuelreichor@gmail.com>
2026-01-11 13:09:25 +0100

    Fix sitemap

--- content.config.ts
+++ content.config.ts
@@ -1,26 +1,31 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content'
+import { asSeoCollection } from '@nuxtjs/seo/content'
 
 export default defineContentConfig({
   collections: {
-    blog: defineCollection({
-      source: '1.blogs/**',
-      type: 'page',
-      schema: z.object({
-        tags: z.array(z.string()),
-        imgPath: z.string(),
-        datePublished: z.date(),
-        readingTime: z.string(),
+    blog: defineCollection(
+      asSeoCollection({
+        source: '1.blogs/**',
+        type: 'page',
+        schema: z.object({
+          tags: z.array(z.string()),
+          imgPath: z.string(),
+          datePublished: z.date(),
+          readingTime: z.string(),
+        })
       })
-    }),
-    docs: defineCollection({
-      source: '2.libraries/**',
-      type: 'page',
-      schema: z.object({
-        badge: z.string().optional(),
-        icon: z.string(),
-        type: z.string(),
+    ),
+    docs: defineCollection(
+      asSeoCollection({
+        source: '2.libraries/**',
+        type: 'page',
+        schema: z.object({
+          badge: z.string().optional(),
+          icon: z.string(),
+          type: z.string(),
+        })
       })
-    }),
+    ),
     snippets: defineCollection({
       source: '_snippets/**',
       type: 'page',
